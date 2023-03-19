22c80d3c137a60eac05f13d5c8a9f7c8d0070742
aleksandersousa <aleksander-ssousa@outlook.com>
2023-03-19 18:02:10 -0300

    finished class node and class node tests

--- src/wisard/node.py
+++ src/wisard/node.py
@@ -1,21 +1,91 @@
-from memory import Memory
+from src.wisard.memory import Memory
 
 
 class Node():
     def __init__(self, tuple_size) -> None:
         self.__tuple_size: int = tuple_size
         self.__memories: list = []
+        self.__threshold: int = 0
+        self.__num_of_disc_patterns: int = 0
+        self.__greater_num_of_disc_patterns: int = 0
 
         self.__init_memory()
+
+    def train(self, addr: str, bleaching=False) -> None:
+        if bleaching:
+            self.__bleaching_train(addr)
+            return
+
+        self.__basic_train(addr)
+
+    def test(self, addr: str, bleaching=False, bleaching_type='simple') -> int:
+        if bleaching:
+            if bleaching_type == 'simple':
+                return self.__simple_bleaching_test(addr)
+            if bleaching_type == 'percentual':
+                return self.__percentual_bleaching_test(addr)
+
+        return self.__basic_test(addr)
+
+    def inc_threshold(self) -> None:
+        self.__threshold += 1
+
+    def dec_threshold(self) -> None:
+        if self.__threshold > 0:
+            self.__threshold -= 1
+
+    def reset_threshold(self) -> None:
+        self.__threshold = 0
+
+    # getters and setters
+
+    def get_threshold(self) -> int:
+        return self.__threshold
 
     def get_memories(self) -> list:
         return self.__memories
 
+    def get_num_of_disc_patterns(self) -> int:
+        return self.__num_of_disc_patterns
+
+    def get_greater_num_of_disc_patterns(self) -> int:
+        return self.__greater_num_of_disc_patterns
+
+    def set_num_of_disc_patterns(self, num_of_disc_patterns: int) -> None:
+        self.__num_of_disc_patterns = num_of_disc_patterns
+
+    def set_greater_num_of_disc_patterns(self, greater_num_of_disc_patterns: int) -> None:
+        self.__greater_num_of_disc_patterns = greater_num_of_disc_patterns
+
     # private
 
+    def __basic_train(self, addr: str) -> None:
+        index = int(addr, 2)
+        self.__memories[index].active()
+
+    def __bleaching_train(self, addr: str) -> None:
+        index = int(addr, 2)
+        self.__memories[index].inc_num_of_acc()
+
+    def __basic_test(self, addr: str) -> int:
+        index = int(addr, 2)
+        return self.__memories[index].get_content()
+
+    def __simple_bleaching_test(self, addr: str) -> int:
+        index = int(addr, 2)
+        return 1 if self.__memories[index].get_content() > self.__threshold else 0
+
+    def __percentual_bleaching_test(self, addr: str) -> int:
+        index = int(addr, 2)
+
+        x = self.__memories[index].get_content() / self.__num_of_disc_patterns
+        y = self.__threshold / self.__greater_num_of_disc_patterns
+
+        return 1 if x > y else 0
+
     def __init_memory(self) -> None:
-        num_of_addrs = self.__tuple_size ** 2
-        rotation = int(num_of_addrs / 2)
+        num_of_addrs = 2 ** self.__tuple_size
+        rotation = num_of_addrs // 2
         addresses = ['' for _ in range(num_of_addrs)]
 
         while rotation != 0:
@@ -27,7 +97,7 @@ class Node():
                     counter = 0
                     value = (value + 1) % 2
 
-                addresses[i] += value
+                addresses[i] += str(value)
                 counter += 1
 
             rotation = int(rotation / 2)
--- tests/wisard/test_node.py
+++ tests/wisard/test_node.py
@@ -1,14 +1,91 @@
-from wisard.node import Node
-
-addresses_out = ['00', '01', '10', '11']
+from src.wisard.node import Node
 
 
 def test_init_memory():
-    node = Node(2)
+    node = Node(3)
 
+    addresses_out = ['000', '001', '010', '011', '100', '101', '110', '111']
     addresses_in = []
 
     for mem in node.get_memories():
         addresses_in.append(mem.get_address())
 
     assert addresses_in == addresses_out
+
+
+def test_inc_threshold():
+    node = Node(2)
+    node.inc_threshold()
+
+    assert node.get_threshold() == 1
+
+
+def test_dec_threshold():
+    node = Node(2)
+    node.inc_threshold()
+    node.dec_threshold()
+
+    assert node.get_threshold() == 0
+
+
+def test_reset_threshold():
+    node = Node(2)
+    node.reset_threshold()
+
+    assert node.get_threshold() == 0
+
+
+def test_basic_train():
+    node = Node(2)
+    addr = '00'
+
+    node.train(addr)
+    index = int(addr, 2)
+
+    assert node.get_memories()[index].get_content() == 1
+
+
+def test_bleaching_train():
+    node = Node(2)
+    addr = '00'
+
+    node.train(addr, bleaching=True)
+    node.train(addr, bleaching=True)
+    index = int(addr, 2)
+
+    assert node.get_memories()[index].get_content() == 2
+
+
+def test_basic_test():
+    node = Node(2)
+    addr = '00'
+
+    node.train(addr)
+
+    assert node.test(addr) == 1
+
+
+def test_simple_bleaching_test():
+    node = Node(2)
+    addr = '00'
+
+    node.train(addr, bleaching=True)
+    node.train(addr, bleaching=True)
+    node.inc_threshold()
+
+    assert node.test(addr, bleaching=True) == 1
+
+
+def test_percentual_bleaching_test():
+    node = Node(2)
+    addr = '00'
+
+    node.train(addr, bleaching=True)
+    node.train(addr, bleaching=True)
+
+    node.inc_threshold()
+
+    node.set_num_of_disc_patterns(1)
+    node.set_greater_num_of_disc_patterns(1)
+
+    assert node.test(addr, bleaching=True, bleaching_type='percentual') == 1
