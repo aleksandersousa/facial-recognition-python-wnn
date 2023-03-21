041ba449bdaa79f1966ed59ceb3d7863d60a8a9d
aleksandersousa <aleksander-ssousa@outlook.com>
2023-03-21 20:58:18 -0300

    fix import path

--- src/file/dataset_file/test_dataset_file.py
+++ src/file/dataset_file/test_dataset_file.py
@@ -4,7 +4,7 @@ from file.dataset_file.dataset_file import DatasetFile
 
 def test_read_training_dataset():
     file_path = str(pathlib.Path().absolute()) + \
-        '\\dataset\\training\\training_face_a_sp.csv'
+        '/dataset/training/training_face_a_sp.csv'
 
     dataset_file = DatasetFile()
     data = dataset_file.read_dataset_file(file_path)
